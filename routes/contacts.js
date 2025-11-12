2247b0820108af2ee2497ed2a6d3bd856972e3c7
MaxTinka <tinkamax03@gmail.com.com>
2025-11-13 01:08:49 +0300

    week3

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,10 +8,19 @@ const {
   deleteContact
 } = require('../controllers/contactsController');
 
+// GET /contacts - Get all contacts
 router.get('/', getAllContacts);
+
+// GET /contacts/:id - Get single contact by ID
 router.get('/:id', getContactById);
+
+// POST /contacts - Create new contact
 router.post('/', createContact);
+
+// PUT /contacts/:id - Update contact by ID
 router.put('/:id', updateContact);
+
+// DELETE /contacts/:id - Delete contact by ID
 router.delete('/:id', deleteContact);
 
 module.exports = router;
