c1993179bdb25e4604a1fd901a1e6ebbb864783b
MaxTinka <tinkamax03@gmail.com>
2026-05-14 19:47:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const contactRoutes = require('./routes/contacts');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/contacts', contactRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+  res.json({ message: 'Contacts API is running!' });
+});
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
@@ -14,8 +21,3 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch(err => console.error('❌ MongoDB error:', err));
-
-// Default route
-app.get('/', (req, res) => {
-  res.json({ message: 'Contacts API is running!' });
-});
